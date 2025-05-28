8e1417cb30142df3b23674422e45bd75f6b8f9d9
aaron-henao <aaronhenao27@gmail.com>
2025-05-28 12:28:20 -0500

    Create model Post and make migrations

--- myfirstblog/models.py
+++ myfirstblog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    tittle = models.CharField(max_length=50)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.tittle
